bb349aadc96175f9586f11a2d1a0451a9dd61711
AhmedReda-662 <ahmed136227reda@gmail.com>
2026-02-14 04:56:11 +0200

    fix security issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CountryRanking from "./components/CountryRanking";
-import CountryDetail from "./components/CountryDetail";
+import CountryDetail from "./components/CountryDetial";
 import AppLayout from "./AppLayout";
 
 const queryClient = new QueryClient({
