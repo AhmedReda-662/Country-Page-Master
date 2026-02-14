aa3419f8aee8abf79718d536f9a369705c9a5506
AhmedReda-662 <ahmed136227reda@gmail.com>
2026-02-14 05:15:24 +0200

    add search bar

--- src/components/CountryRanking.jsx
+++ src/components/CountryRanking.jsx
@@ -1,8 +1,13 @@
+import SearchBar from "../features/countryranking/searchCountry";
+
 export default function CountryRanking() {
   return (
     <div>
-      <div className="status">
-        <div>Found 195 countries</div>
+      <div className="flex items-center justify-between px-3">
+        <div className="text-gray-secondary font-medium">
+          Found 195 countries
+        </div>
+        <SearchBar />
       </div>
     </div>
   );
